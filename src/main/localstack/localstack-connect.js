9dcaae2e499c11ff68450044bb986a2116ab0d62
Barrett Cervenka <barrett.cervenka@northscaler.com>
2020-07-15 16:49:12 +0000

    Add ability to specify a docker label for use with local stack

--- src/main/localstack/localstack-connect.js
+++ src/main/localstack/localstack-connect.js
@@ -5,13 +5,16 @@ const startLocalstack = require('./start-localstack')
 
 const fn = async ({
   containerName = fn.defaultContainerName,
-  port = fn.defaultPort
+  port = fn.defaultPort,
+  healthPort = fn.defaultHealthPort,
+  containerLabel = fn.defaultContainerLabel
 } = {}) => {
-  await startLocalstack({ scriptArgs: [containerName, port] })
+  await startLocalstack({ scriptArgs: [containerName, port, healthPort, containerLabel] })
 }
 
 fn.defaultPort = parseInt(fs.readFileSync(`${__dirname}/default-localstack-test-port`))
 fn.defaultHealthPort = parseInt(fs.readFileSync(`${__dirname}/default-localstack-test-health-port`))
 fn.defaultContainerName = fs.readFileSync(`${__dirname}/default-localstack-test-container`)
+fn.defaultContainerLabel = fs.readFileSync(`${__dirname}/default-localstack-test-container-label`)
 
 module.exports = fn
