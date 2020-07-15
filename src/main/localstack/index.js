a485cba19137b17762fb558f5f9766c555bbd5a9
Barrett Cervenka <barrett.cervenka@northscaler.com>
2020-07-15 11:16:28 -0500

    export defaultHealthPort

--- src/main/localstack/index.js
+++ src/main/localstack/index.js
@@ -5,6 +5,7 @@ const localstackConnect = require('./localstack-connect')
 module.exports = {
   localstackConnect,
   defaultPort: localstackConnect.defaultPort,
+  defaultHealthPort: localstackConnect.defaultHealthPort,
   defaultContainerName: localstackConnect.defaultContainerName,
   defaultContainerLabel: localstackConnect.defaultContainerLabel
 }
